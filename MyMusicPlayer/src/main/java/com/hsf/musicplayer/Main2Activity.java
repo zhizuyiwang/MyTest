76b20741564cff4842ba858c878e9c455c325aa2
zizuyiwang <1175432154@qq.com>
2016-07-21 10:04:18 +0800

    modify

--- MyMusicPlayer/src/main/java/com/hsf/musicplayer/Main2Activity.java
+++ MyMusicPlayer/src/main/java/com/hsf/musicplayer/Main2Activity.java
@@ -2,6 +2,7 @@ package com.hsf.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        WindowManager wm = getWindowManager();
     }
 }
