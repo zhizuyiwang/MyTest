360ebd4dff58a982768b880e0c7bca6088988de0
zizuyiwang <1175432154@qq.com>
2016-08-22 18:51:46 +0800

    我的音乐添加背景图片

--- MyMusic/src/main/java/com/hsf/music/fragment/Fragment1.java
+++ MyMusic/src/main/java/com/hsf/music/fragment/Fragment1.java
@@ -1,10 +1,16 @@
 package com.hsf.music.fragment;
 
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.View;
 
 /**
  * Created by Administrator on 2016/8/2.
  */
 public class Fragment1 extends FragmentActivity{
-
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
 }
